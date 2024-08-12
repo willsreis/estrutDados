4be356bbe99d83ef6efe8f6abad4146e40485d02
willsreis <williamsilva.r@gmail.com>
2024-08-12 09:11:46 -0300

    alteracao e reestruturacao de diretorios e arquivos

---
+++ 02/js/variables.js
@@ -0,0 +1,14 @@
+var num = 1; // {1}
+num = 3; // {2}
+var price = 1.5; // {3}
+var myName = 'Packt'; // {4}
+var trueValue = true; // {5}
+var nullVar = null; // {6}
+var und; // {7}
+
+console.log('num: ' + num);
+ console.log('myName: ' + myName);
+ console.log('trueValue: ' + trueValue);
+ console.log('price: ' + price);
+ console.log('nullVar: ' + nullVar);
+ console.log('und: ' + und);
