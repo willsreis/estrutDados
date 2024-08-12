8d0a2b9640c1af0b99aebb223fa43a998ddfe6c9
willsreis <williamsilva.r@gmail.com>
2024-08-12 11:08:52 -0300

    adicionando file oepradores.js

--- 02/js/variables.js
+++ 02/js/variables.js
@@ -12,3 +12,29 @@ console.log('num: ' + num);
  console.log('price: ' + price);
  console.log('nullVar: ' + nullVar);
  console.log('und: ' + und);
+
+
+
+
+var myVariable = 'global';
+myOtherVariable = 'global';
+
+function myFunction() {
+var myVariable = 'local';
+return myVariable;
+}
+
+function myOtherFunction() {
+myOtherVariable = 'local';
+return myOtherVariable;
+}
+
+console.log(myVariable); //{1} global
+console.log(myFunction()); //{2}local
+console.log(myVariable); // global
+console.log(myOtherVariable); //{3} global
+console.log(myOtherFunction()); //{4} local
+console.log(myOtherVariable); //{5}local
+
+
+
