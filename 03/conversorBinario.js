e27f23ddc8f1277a55c5730fd404a596995b766c
willsreis <williamsilva.r@gmail.com>
2024-09-09 13:07:45 -0300

    fix conversor binario

--- 03/conversorBinario.js
+++ 03/conversorBinario.js
@@ -1,17 +1,42 @@
-function decimaBinario(decNumber) {
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    push(element) {
+        this.items.push(element);
+    }
+
+    pop() {
+        return this.items.pop();
+    }
+
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+}
+
+function decimalToBinary(decNumber) {
     const remStack = new Stack();
     let number = decNumber;
     let rem;
-    let binarioString = '';
-    while (number > 0) {
-        rem = Math.floor(number % 2);
+    let binaryString = '';
+
+     while (number > 0) {
+        rem = number % 2;
         remStack.push(rem);
         number = Math.floor(number / 2);
     }
+ 
     while (!remStack.isEmpty()) {
-        binarioString += remStack.pop().toString();
-
+        binaryString += remStack.pop().toString();
     }
-retun binarioString;
+
+    return binaryString;        
 }
-console.log(decimaBinario(233));
+
+console.log(decimalToBinary(10));       
